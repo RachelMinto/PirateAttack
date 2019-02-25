2b0c8f5b928ea13fbe26c7c2b0d716ded218ed47
Rachel Minto <rminto@wistia.com>
2019-02-25 08:24:49 -0500

    First scene and Shooting Cannon Prefab.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField] float movementSpeed = 5f;
+
+    void Update () {
+        transform.Translate(Vector3.down * movementSpeed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject projectileOrigin;
+    GameObject projectileParent;
+
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
+	void Start () {
+        CreateProjectileParent();
+        Fire();
+	}
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
+	
+    void Fire () {
+        GameObject newProjectile = Instantiate(
+            projectile,
+            projectileOrigin.transform.position,
+            Quaternion.identity
+        ) as GameObject;
+
+        newProjectile.transform.parent = projectileParent.transform;
+    }
+}
