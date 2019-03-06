98a7456a4982438370d438bf9acf548cdf0a1c91
Rachel Minto <rminto@wistia.com>
2019-03-06 08:17:32 -0500

    Ship prefabs that follow patrol path in waves.

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+    private Patroller agent; // don't need?
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+        Debug.Log("IN START");
+        Debug.Log(transform.position);
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            Debug.Log(targetPosition);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            Debug.Log(transform.position);
+            if (transform.position == targetPosition)
+            {
+                Debug.Log("Incrementing waypoint index");
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+}
